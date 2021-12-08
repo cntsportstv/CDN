5d0eeaf68d346e3f04e3a451e03eb4c530cf9e15
cntsportstv <95767180+cntsportstv@users.noreply.github.com>
2021-12-08 11:44:22 -0500

    Add files via upload

---
+++ arlinablock.js
@@ -0,0 +1,9 @@
+!function(){function f(){var a=document.createElement("div");a.id="arlinablock";a.innerHTML='
+<div class="inner">
+  <div class="header">
+    <h2>Ad Blocker Detected</h2>
+  </div>
+  <div class="isi">
+    <p>Please consider supporting us by disabling your ad blocker</p>
+  </div>
+</div>'; document.body.append(a);document.body.style.overflow="hidden";var b=a.querySelectorAll("button");a.querySelector(".close");var d=a.querySelectorAll(".fixblock &gt; div");for(a=0;a
